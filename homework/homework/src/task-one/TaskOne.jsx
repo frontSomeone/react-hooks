8365d435fb52b1a78ce858e8be157db398b98acc
Roman <halikov_99@bk.ru>
2024-10-21 11:21:52 +0300

    project with errors

--- homework/homework/src/task-one/TaskOne.jsx
+++ homework/homework/src/task-one/TaskOne.jsx
@@ -1,14 +1,7 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import './TaskOne.css';
 
-function TaskOne() {
-    /**
-     * Вынесите эти стейты в свой хук, все изменения полей должны валидирвоаться по разным правилам:
-     * firstName, lastName - не могут быть пустыми
-     * email - должен совпадать с паттерном email, оп которому стандартный email адрес- валидный, а test или @some или some@te - будут не валидны
-     * password - должен быть не меньше 5 символов и должен включать в себя цифры и сепц символы (%$@ и т.д.)
-     * confirmPassword - должен совпадать с password
-     * */
+function useForm() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
@@ -16,8 +9,31 @@ function TaskOne() {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
 
-    // Ваш хук должен возвращать фукцию которую будет использовать форма для сабмита данных
-    const onSubmitHandle = (event) => {
+    const checkFirstName = (e) => {
+        if (e === "") {
+        setError("Поле firstName и lastName не могут быть пустыми");
+    } else {
+        setError("");
+    }};
+    const checkLastName = (e) => {
+        if (e === "") {
+        setError("Поле firstName и lastName не могут быть пустыми");
+    } else {
+        setError("");
+    }};
+    const checkEmail = (e) => {
+        if (!/^[A-Z0-9]+\@[A-Z0-9]+\.[A-Z0-9]{2,}$/i.test(e)) {
+        setError("Можно ввести только латинские буквы, цифры и обязательно должны присутствовать символы «@» и «.», после «.» должно быть не менее 2-х символов");
+    } else {
+        setError("");
+    }};
+    const checkPass = () => {
+        if (password !== confirmPassword) {
+        setError('Пароли не совпадают!');
+    } else {
+        setError("");
+    }};
+        const onSubmitHandle = useCallback((event) => {
         event.preventDefault();
 
         const {
@@ -28,24 +44,45 @@ function TaskOne() {
             confirmPassword,
         } = event;
 
-        // Здесь вы можете обрабатывать логику отправки формы,
-        // например, вызвать ваш API для отправки данных формы
-
-        // После успешной отправки формы, очистите все поля
         setFirstName('');
         setLastName('');
         setEmail('');
         setPassword('');
         setConfirmPassword('');
 
-
-        // И используйте alert, чтобы показать результат
         alert(JSON.stringify({firstName,
             lastName,
             email,
             password,
             confirmPassword,}));
-    };
+        
+        }, []);
+        return {firstName, lastName, email, password, confirmPassword, error, checkFirstName, checkLastName, checkEmail, checkPass, onSubmitHandle};
+    }
+
+function TaskOne(checkFirstName, checkLastName, checkEmail, checkPass, onSubmitHandle) {
+    const {firstName, lastName, email, password, confirmPassword, error} = useForm();
+    /**
+     * Вынесите эти стейты в свой хук, все изменения полей должны валидирвоаться по разным правилам:
+     * firstName, lastName - не могут быть пустыми
+     * email - должен совпадать с паттерном email, оп которому стандартный email адрес- валидный, а test или @some или some@te - будут не валидны
+     * password - должен быть не меньше 5 символов и должен включать в себя цифры и сепц символы (%$@ и т.д.)
+     * confirmPassword - должен совпадать с password
+     * */
+    
+
+    // Ваш хук должен возвращать фукцию которую будет использовать форма для сабмита данных
+    
+
+        // Здесь вы можете обрабатывать логику отправки формы,
+        // например, вызвать ваш API для отправки данных формы
+
+        // После успешной отправки формы, очистите все поля
+        
+
+
+        // И используйте alert, чтобы показать результат
+        
 
     // TODO: реализуйте пользовательский хук для валидации
     // const submitForm = useSubmitForm(onSubmitHandle);
@@ -55,16 +92,16 @@ function TaskOne() {
         <div className="form-container">
             <div className="error-message">{error}</div>
             <form onSubmit={onSubmitHandle}> {/* Измените здесь на submitForm, когда он будет готов */}
-                <input type="text" name="firstName" placeholder="First Name" className="form-input"
-                       onChange={(e) => setFirstName(e.target.value)} value={firstName}/>
+                <input  type="text" name="firstName" placeholder="First Name" className="form-input"
+                       onChange={(e) => checkFirstName(e.target.value)} value={firstName}/>
                 <input type="text" name="lastName" placeholder="Last Name" className="form-input"
-                       onChange={(e) => setLastName(e.target.value)} value={lastName}/>
+                       onChange={(e) => checkLastName(e.target.value)} value={lastName}/>
                 <input type="email" name="email" placeholder="Email" className="form-input"
-                       onChange={(e) => setEmail(e.target.value)} value={email}/>
+                       onChange={(e) => checkEmail(e.target.value)} value={email}/>
                 <input type="password" name="password" placeholder="Password" className="form-input"
-                       onChange={(e) => setPassword(e.target.value)} value={password}/>
-                <input type="password" name="confirmPassword" placeholder="Confirm Password" className="form-input"
-                       onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}/>
+                       onChange={(e) => checkPass(e.target.value)} value={password}/>
+                {/* <input type="password" name="confirmPassword" placeholder="Confirm Password" className="form-input"
+                       onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}/> */}
                 <button type="submit" className="form-button">Register</button>
             </form>
         </div>
