1c889644774ec072fc1c64b33da91ac0537382b6
Roman <halikov_99@bk.ru>
2024-11-18 23:56:14 +0300

    error loading

--- homework/homework/src/task-three/TaskThree.jsx
+++ homework/homework/src/task-three/TaskThree.jsx
@@ -1,32 +1,48 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import { debounce, get } from 'lodash';
+import React, {useEffect, useState} from 'react';
+import { debounce } from 'lodash';
 import './TaskThree.css';
 
 function useFetch() {
     const [search, setSearch] = useState();
     const [posts, setPosts] = useState([]);
+    const [load, setLoad] = useState();
+    console.log(load);
+    console.log(posts);
+    console.log(search);
+    
     // функция для получения данных с Mock API
+    let controller = new AbortController();
     useEffect(() => {
         if (search) {
-            let controller = new AbortController();
             fetchData(search, controller);
+            setLoad("Загрузка");
+        }
+        if (posts) {
+            setLoad("");
+        }
+        if (search === "") {
+            setPosts([]);
         }
     }, [search]);
     
-    const fetchData = useCallback(debounce(async(search, controller) => {
+        const fetchData = debounce(async(search, controller) => {
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts?search=${search}`, {signal: controller.signal});
+        console.log(response);
+        
         setPosts(await response.json())
-    }, 500), []);
-    return {setSearch, posts}
+    }, 500);
+    
+    return {setSearch, posts, load}
 }
 
 
 export default function TaskThree() {
-    const {setSearch, posts} = useFetch();
+    const {setSearch, posts, load} = useFetch();
     return (
         <div className="TaskThree">
             <input type="text" onChange={(event) => setSearch(event.target.value)} placeholder="Search posts"/>
             <h1>Posts</h1>
+            <div>{load}</div>
             <ul>
                 {posts.map(item => <li key={item.id}>{item.title}</li>)}
             </ul>
